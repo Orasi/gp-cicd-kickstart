b0b42f39e8a2c4395d7fb60ecd0217489962de2f
bgree13 <mrbdg@hotmail.com>
2018-09-28 11:58:43 -0400

    removed sudo

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo yum check-update
-sudo yum install -y git
-sudo curl -fsSL https://get.docker.com/ | sh
-sudo usermod -aG docker $(whoami)
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
-sudo yum install epel-release -y
-sudo yum install -y python-pip -y
-sudo pip install docker-compose 
-sudo yum upgrade python* -y
-sudo git clone https://github.com/Orasi/gp-cicd-kickstart.git
+yum check-update
+yum install -y git
+curl -fsSL https://get.docker.com/ | sh
+usermod -aG docker $(whoami)
+systemctl enable docker.service
+systemctl start docker.service
+yum install epel-release -y
+yum install -y python-pip -y
+pip install docker-compose 
+yum upgrade python* -y
+git clone https://github.com/Orasi/gp-cicd-kickstart.git
