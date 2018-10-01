af85c769b98d6381bb4683754eabc33f68efb063
Breon Green <bgreen@MacBook-Pro.local>
2018-10-01 01:27:53 -0400

    updated provision.sh

--- provision.sh
+++ provision.sh
@@ -7,4 +7,5 @@ timedatectl set-timezone America/New_York
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install puppet-agent
 echo "1 * * * * root /opt/puppetlabs/bin/puppet agent -t >>/var/log/puppet-agent.log 2>&1" >> /etc/cron.d/puppet-agent
+echo "runinterval=20s" | tee -a /etc/puppetlabs/puppet/puppet.conf
 systemctl restart crond
